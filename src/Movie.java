7b44eb82c3c455bcae3499ddc9cb0dbf43803e37
GuntzelT <taysonkaue98@gmail.com>
2026-05-22 10:59:07 -0300

    Movie Class and Main Object

---
+++ src/Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+    String title;
+    int releaseDate;
+    double rating;
+    int duration;
+    int budget;
+    int totalReviews;
+    boolean isAvailableOnStreaming;
+
+}
